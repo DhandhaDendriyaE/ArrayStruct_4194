b5dd7075b6e32eae91750915a7a8636186c0375c
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-04-15 11:11:43 +0700

    membuat index array skor, nilai dan nama

--- array.cpp
+++ array.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+
 // deklarasi array tanpa ukuran, wajib langsung di isi data 
 int nilai []={1,2,3};
 //deklarasi array dengan ukuran, dan diberi nilai awal
