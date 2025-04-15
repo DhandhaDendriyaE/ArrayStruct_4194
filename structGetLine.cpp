cc3ddb70ddc1ea887cd3d472b4bc6dab5cc72969
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-04-15 11:24:41 +0700

    membuat struktur data mahasiswa

--- structGetLine.cpp
+++ structGetLine.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 using namespace std;
 
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
 
 struct mahasiswa
-
 {
     string nim;
     string nama;
-    string alamat; 
+    detailAlamat alamat;
 };
 
 int main(){
     mahasiswa mhs;
 
-    mhs.nim = "20220140194";
-    mhs.nama = "Dhandha Dendriya";
-    mhs.alamat = "balecatur";
-
     cout << "Masukan NIM = ";
     cin >> mhs.nim;
+    cin.ignore();
     cout << "Masukan NAMA = ";
     cin >> mhs.nama;
-    cout << "Masukan Alamat = ";
-    cin >> mhs.alamat;
+    cout << "Masukan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukan Alamat Kota";
+    cin >> mhs.alamat.kota;
 
-    //menampilkan data struct
     cout << endl;
     cout << "NIM = " << mhs.nim << endl;
     cout << "Nama = " << mhs.nama << endl;
-    cout << "Alamat = " << mhs.alamat << endl;
+    cout << "Desa = " << mhs.alamat.desa << endl;
+    cout << "Kota = " << mhs.alamat.kota << endl;
     
-
-};
+}
